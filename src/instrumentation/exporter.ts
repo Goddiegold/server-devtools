597141a520dc4662110a92b08082adfaa002d378
Goddiegold <gehikhamhen247@gmail.com>
2026-08-25 22:12:59 +0100

    updates: extendedd instrumentation

--- src/instrumentation/exporter.ts
+++ src/instrumentation/exporter.ts
@@ -8,34 +8,35 @@ import TraceStore from "../core/trace-store";
 export default class ServerDevToolsExporter implements SpanExporter {
     private readonly spanMapper = new SpanMapper();
 
-     constructor(
-    private readonly traceAssembler: TraceAssembler,
-    private readonly traceStore: TraceStore,
-  ) {}
+    constructor(
+        private readonly traceAssembler: TraceAssembler,
+        private readonly traceStore: TraceStore,
+    ) { }
 
     export(
         spans: ReadableSpan[],
         resultCallback: (result: ExportResult) => void,
-    ) {
-
+    ): void {
         for (const span of spans) {
-            console.log(`Exporting span: ${span.name}`);
-
-            const context = span.spanContext();
-            console.log(`Span context: traceId=${context.traceId}, spanId=${context.spanId}`);
-
             const devToolsSpan = this.spanMapper.map(span);
+
             this.traceAssembler.addSpan(devToolsSpan);
 
-            const trace = this.traceAssembler.getTrace(devToolsSpan.traceId);
+            const trace = this.traceAssembler.getTrace(
+                devToolsSpan.traceId,
+            );
+
             if (trace) {
                 this.traceStore.add(trace);
+                //    console.dir(this.traceStore.get(trace.traceId), {
+                //                     depth: null,
+                //                 });
             }
-            
-            console.log(devToolsSpan);
         }
 
-        resultCallback({ code: ExportResultCode.SUCCESS });
+        resultCallback({
+            code: ExportResultCode.SUCCESS,
+        });
     }
 
     async shutdown(): Promise<void> {
--- src/instrumentation/instrumentation.ts
+++ src/instrumentation/instrumentation.ts
@@ -3,15 +3,22 @@ import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import ServerDevToolsExporter from "./exporter";
+import TraceAssembler from "../core/trace-assembler";
+import TraceStore from "../core/trace-store";
 
 
 export class Instrumentation {
     private readonly oTelSdk: NodeSDK;
+    readonly traceStore: TraceStore;
 
     constructor() {
+        const traceAssembler = new TraceAssembler();
+        const traceStore = new TraceStore();
+        this.traceStore = traceStore;
+
         this.oTelSdk = new NodeSDK({
             spanProcessors: [
-                new SimpleSpanProcessor(new ServerDevToolsExporter()),
+                new SimpleSpanProcessor(new ServerDevToolsExporter(traceAssembler, traceStore)),
             ],
             instrumentations: [
                 new HttpInstrumentation(),
